3b2746808a93cc5fda8c8812db70319b334ef6aa
romeo <romeolacerdafarias@gmail.com>
2024-10-17 10:22:50 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+
+export default [
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "class-methods-use-this": "off",
+        },
+    },
+];
