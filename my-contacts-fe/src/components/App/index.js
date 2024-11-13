2f06592f77d057df302fbf7ed474fcabf3d31c0a
romeo <romeolacerdafarias@gmail.com>
2024-11-12 21:26:32 -0300

    begin frontend

---
+++ my-contacts-fe/src/components/App/index.js
@@ -0,0 +1,26 @@
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../../assets/styles/global';
+
+import defaultTheme from '../../assets/styles/themes/default';
+import Routes from '../../Routes';
+import Header from '../Header';
+import { Container } from './styles';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
